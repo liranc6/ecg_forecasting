fix normalization and maybe other things

liran_project/mrdiff/exp_main.py
@@ -1,33 +1,6 @@
-import os
-import sys
-import time
-import wandb
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch import optim
-from torch.optim import lr_scheduler
-from torch.utils.data import DataLoader
-
-import random
-import numpy as np
 import yaml
-from box import Box
-from pprint import pprint
-import wandb
-from tqdm.notebook import tqdm
-from datetime import timedelta
-from collections import defaultdict
-
-import warnings
-warnings.filterwarnings('ignore')
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-from einops import rearrange, repeat
-from einops.layers.torch import Rearrange
+import sys
+from torch.utils.data import DataLoader, SubsetRandomSampler
 
 CONFIG_FILENAME = '/home/liranc6/ecg_forecasting/liran_project/mrdiff/src/config_ecg.yml'
 
@@ -43,6 +16,7 @@ sys.path.append(ProjectPath)
 from liran_project.mrdiff.src.parser import parse_args
 from liran_project.utils.dataset_loader import SingleLeadECGDatasetCrops_mrDiff as DataSet
 from liran_project.utils.util import ecg_signal_difference, check_gpu_memory_usage
+from liran_project.utils.common import *
 
 # Add the directory containing the exp module to the sys.path
 exp_module_path = os.path.join(ProjectPath, 'mrDiff')
@@ -107,10 +81,14 @@ class Exp_Main(Exp_Basic):
         label_window_size = config_dict['training']['sequence']['label_len']  # minutes * seconds * fs
         window_size = context_window_size+label_window_size
         
-        if flag == 'train' or flag == 'val':
+        if flag == 'train':
             data_path = config['paths']['train_data']
             start_patiant = config['training']['patients']['start_patient']
             end_patiant = config['training']['patients']['end_patient']
+        elif flag == 'val':
+            data_path = config['paths']['val_data']
+            start_patiant = config['validation']['patients']['start_patient']
+            end_patiant = config['validation']['patients']['end_patient']
         elif flag == 'test':
             data_path = config['paths']['test_data']
             start_patiant = config['testing']['patients']['start_patient']
@@ -123,7 +101,7 @@ class Exp_Main(Exp_Basic):
                                 end_patiant=end_patiant,
                                 data_with_RR=True,
                                 return_with_RR=True,
-                                # normalize_method = 'z_score',
+                                normalize_method = self.args.data.norm_method,
                                 )
         
         if flag == 'test':
@@ -131,23 +109,36 @@ class Exp_Main(Exp_Basic):
             drop_last = False
             batch_size = self.args.optimization.test_batch_size
             freq=self.args.data.freq
+            sampler = self._get_nth_sampler(dataset, n=8)
         elif flag=='pred':
             shuffle_flag = False 
             drop_last = False 
             batch_size = 1
             freq=self.args.detail_freq
-        else: # train or val
+            sampler = None
+        elif flag == 'train':
             shuffle_flag = True
             drop_last = True
             batch_size = self.args.optimization.batch_size
             freq=self.args.data.freq
+            sampler = None
+        elif flag == 'val':
+            shuffle_flag = False
+            drop_last = False
+            batch_size = self.args.optimization.batch_size
+            freq=self
+            sampler = self._get_nth_sampler(dataset, n=8)
+        else:
+            raise ValueError("Invalid flag")
+        
 
         data_loader = DataLoader(
             dataset,
             batch_size=batch_size,
             shuffle=shuffle_flag,
             num_workers=config['hardware']['num_workers'],
-            drop_last=drop_last
+            drop_last=drop_last,
+            sampler=sampler
         )
         
         self.datasets[flag] = dataset
@@ -156,6 +147,21 @@ class Exp_Main(Exp_Basic):
         
         return dataset, data_loader
     
+    def _get_nth_sampler(self, dataset, n=1, seed=None):
+        
+        random.seed(seed)
+        
+        # Calculate indices of every nth batch
+        indices = list(range(0, len(dataset), 1))
+        random.shuffle(indices)
+        
+        # take every nth index
+        indices = indices[::n]
+        
+        random.shuffle(indices)
+        
+        return SubsetRandomSampler(indices)
+
     def vali(self, vali_data, vali_loader, pretrain=False):
         
         total_loss = []

liran_project/mrdiff/main_default.py
@@ -1,33 +1,5 @@
-import argparse
-import os
-import sys
-import time
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch import optim
-from torch.optim import lr_scheduler
-from torch.utils.data import DataLoader
-
-import random
-import numpy as np
 import yaml
-from box import Box
-from pprint import pprint
-import wandb
-from tqdm import tqdm
-from datetime import timedelta
-from collections import defaultdict
-
-import warnings
-warnings.filterwarnings('ignore')
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-from einops import rearrange, repeat
-from einops.layers.torch import Rearrange
+import sys
 
 CONFIG_FILENAME = '/home/liranc6/ecg_forecasting/liran_project/mrdiff/src/config_ecg.yml'
 
@@ -44,6 +16,9 @@ from liran_project.mrdiff.src.parser import parse_args
 from liran_project.utils.dataset_loader import SingleLeadECGDatasetCrops_mrDiff as DataSet
 from liran_project.utils.util import ecg_signal_difference, check_gpu_memory_usage
 from liran_project.mrdiff.exp_main import Exp_Main
+from liran_project.mrdiff.src.parser import Args
+from liran_project.utils.common import *
+
 
 # Add the directory containing the exp module to the sys.path
 exp_module_path = os.path.join(ProjectPath, 'mrDiff')
@@ -57,7 +32,6 @@ from mrDiff.exp.exp_basic import Exp_Basic
 from mrDiff.models_diffusion import DDPM
 from mrDiff.utils.tools import EarlyStopping, adjust_learning_rate, visual
 from mrDiff.utils.metrics import metric
-from liran_project.mrdiff.src.parser import Args
 
 
 def main():

liran_project/utils/dataset_loader.py
@@ -1,15 +1,5 @@
-from pickle import FALSE
-from torch.utils.data import Dataset
-import h5py
-import numpy as np
-import torch
-import bisect
-from collections import OrderedDict
-import os
-import sys
 import yaml
-from sklearn.preprocessing import StandardScaler
-from tqdm import tqdm
+import sys
 
 CONFIG_FILENAME = '/home/liranc6/ecg_forecasting/mrDiff/configs/config.yml'
 
@@ -21,6 +11,8 @@ with open(CONFIG_FILENAME, 'r') as file:
 # Add the parent directory to the sys.path
 ProjectPath = config['project_path']
 sys.path.append(ProjectPath)
+
+from liran_project.utils.common import *
 
 
 
@@ -257,41 +249,37 @@ class SingleLeadECGDatasetCrops_mrDiff(Dataset):
             
     def _get_normalization_statistics(self, filename):
         mean = 0
-        std = 0
+        welford = WelfordOnline()
         max_val = np.NINF
         min_val = np.Inf
+
         with h5py.File(filename, 'r') as h5_file:
             num_keys = len(self.keys)
-            pbar_keys = tqdm(self.keys , total=num_keys)
-            for i, key in enumerate(pbar_keys):
+            pbar_keys = tqdm(self.keys, total=num_keys)
+            
+            for key in pbar_keys:
                 pbar_keys.set_description(f"Reading {key}")
-                if self.data_with_RR:
-                    data = h5_file[key][()][:, 0, :]
-                else:
-                    data = h5_file[key][()]
-                mean += np.mean(data, axis=0)
-                std += np.std(data, axis=0)
+                data = h5_file[key][()][:, 0, :] if self.data_with_RR else h5_file[key][()]
+                    
+                curr_num_samples = data.shape[0]
+                welford.add_points(data)
+                total_num_samples += curr_num_samples
+
                 max_val = max(max_val, np.max(data))
                 min_val = min(min_val, np.min(data))
-                
-                if i >10:
-                    break
-            
-            scale = max_val - min_val + 1e-8
-            mean /= num_keys
-            std /= num_keys
-            std = np.where(std == 0, 1, std)
-            
-        return {"max": max_val, "min": min_val, "scale": scale, "mean": mean, "std": std}
-            
-    def inverse_transform(self, data):
+
+        # Assert that total_num_samples is greater than zero
+        assert total_num_samples > 0, "Total number of samples is zero. Check the data."
         
-    	return de_normalized(data, self.normalize_method, self.norm_statistic)
-    
-    
+        mean, std = welford.get_mean_and_stddev()
+
+        # Avoid zero std dev
+        std = np.where(std == 0, 1, std)  # Log a warning if std is 1
+        return mean, std, max_val, min_val     
+            
 def normalized(data, normalize_method, norm_statistics):
     if normalize_method == 'min_max':
-        scale = norm_statistics['scale']
+        scale = norm_statistics['max'] - norm_statistics['min']
         data = (data - norm_statistics['min']) / scale
     elif normalize_method == 'z_score':
         mean = norm_statistics['mean']
@@ -301,10 +289,71 @@ def normalized(data, normalize_method, norm_statistics):
 
 def de_normalized(data, normalize_method, norm_statistics):
     if normalize_method == 'min_max':
-        scale = norm_statistics['scale']
+        scale = norm_statistics['max'] - norm_statistics['min']
         data = data * scale + norm_statistics['min']
     elif normalize_method == 'z_score':
         mean = norm_statistics['mean']
         std = norm_statistics['std']
         data = data * std + mean
     return data
+
+
+class WelfordOnline:
+    def __init__(self):
+        self.n = 0
+        self.mean = None
+        self.variance = None
+
+    def add_points(self, data):
+        # Convert data to a NumPy array for easy manipulation
+        data = np.asarray(data)
+        k = data.shape[0]  # Number of new data points
+
+        if k == 0:
+            return  # No points to add
+        
+        # Initialize mean and variance if this is the first addition
+        if self.mean is None:
+            self.mean = np.zeros(data.shape[1])  # Assuming data has columns
+            self.variance = np.zeros(data.shape[1])  # Assuming data has columns
+
+        # Update the count
+        self.n += k
+        
+        # Calculate the mean increment along axis=0
+        mean_increment = np.mean(data, axis=0)
+
+        # Update the overall mean
+        old_mean = self.mean.copy()
+        self.mean += (mean_increment - self.mean) * (k / self.n)
+
+        # Update the variance for each dimension
+        for i in range(data.shape[1]):
+            self.variance[i] += np.sum((data[:, i] - old_mean[i]) * (data[:, i] - self.mean[i]))
+
+    def get_variance(self):
+        if self.n < 2:
+            return float('nan')  # Not enough data for variance
+        return self.variance / (self.n - 1)  # Sample variance
+
+    def get_population_variance(self):
+        if self.n == 0:
+            return float('nan')  # Not enough data
+        return self.variance / self.n  # Population variance
+    
+    def get_stddev(self):
+        return np.sqrt(self.get_variance())
+    
+    def get_population_stddev(self):
+        return np.sqrt(self.get_population_variance())
+    
+    def get_mean(self):
+        return self.mean
+    
+    def get_mean_and_stddev(self):
+        return self.mean, self.get_population_stddev()
+
+    def get_mean(self):
+        return self.mean  
+    
+    

liran_project/utils/util.py
@@ -8,7 +8,7 @@ import wfdb
 import pandas as pd
 from fastdtw import fastdtw
 import neurokit2 as nk
-from tqdm import tqdm
+# from tqdm import tqdm
 from scipy.spatial import distance
 
 SAMPLING_RATE = 250
@@ -834,4 +834,3 @@ def check_gpu_memory_usage(device_id=0):
     else:
         print("CUDA is not available.")
         return None
-    

mrDiff/models_diffusion/DDPM_diffusion_worker.py
@@ -3,7 +3,6 @@ from typing import List, Optional, Tuple, Union
 from typing import Any, Dict
 from functools import partial
 from inspect import isfunction
-from tqdm import tqdm
 import math
 import torch
 import torch.nn as nn
@@ -16,6 +15,11 @@ import importlib
 
 from torch import optim
 from utils.losses import mape_loss, mase_loss, smape_loss
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+from liran_project.utils.common import *
 
 def exists(x):
     return x is not None