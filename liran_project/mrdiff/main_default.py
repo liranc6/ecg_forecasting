d273a1140c778b3817f47df7f59758a46e2e8197
Liran Cohen <liranc6@campus.technion.ac.il>
2024-11-11 15:40:30 +0200

    running, fixed wandb

--- liran_project/mrdiff/main_default.py
+++ liran_project/mrdiff/main_default.py
@@ -6,6 +6,7 @@ from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping, ModelSummary
 from pytorch_lightning.strategies import DDPStrategy, FSDPStrategy
+from pytorch_lightning.utilities.rank_zero import rank_zero_only
 
 CONFIG_FILENAME = '/home/liranc6/ecg_forecasting/liran_project/mrdiff/src/config_ecg.yml'
 
@@ -68,47 +69,15 @@ class CustomEarlyStopping(EarlyStopping):
                         print(f"Improved {metric_name}: {metric_value:.4f}")
             super(CustomEarlyStopping, self).on_validation_end(trainer, pl_module)       
         
-                                
-def main():
-    args = parse_args(CONFIG_FILENAME)
-    
-    pprint(vars(args))
-    
-    # Convert Box object to dictionary
-    config_dict = args.configs.to_dict()
-
-    # Access the configuration values using dictionary syntax
-    random_seed = config_dict['general']['random_seed']
-    tag = config_dict['general']['tag']
-    dataset = config_dict['general']['dataset']
-    features = config_dict['general']['features']
-
-    learning_rate = config_dict['optimization']['learning_rate']
-    batch_size = config_dict['optimization']['batch_size']
-
-    context_len = config_dict['training']['sequence']['label_len']
-    label_len = config_dict['training']['sequence']['pred_len']
-    model = config_dict['training']['model_info']['model']
-    pred_len = config_dict['training']['sequence']['pred_len']
-    iterations = config_dict['training']['iterations']['itr']
-
-    inverse = config_dict['data']['inverse']
-        
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True  # Can change it to False --> default: False
-    torch.backends.cudnn.enabled = True
-    
-    if args.wandb.resume in ["allow", "must", "auto"] and args.resume_exp.resume is not True:
-        raise ValueError("The 'resume' argument must be set to True in order to resume a wandb run.")
-    
-    # wandb
+@rank_zero_only
+def init_wandb_logger(args):
     wandb_project_name = args.wandb.project
     wandb_id = args.wandb.id if args.wandb.id != "None" else None
     wandb_mode = args.wandb.mode if args.wandb.mode != "None" else "online"
     wandb_resume = args.wandb.resume if args.wandb.resume != "None" else None
-    
+
     wandb_logger = WandbLogger(project=wandb_project_name, id=wandb_id, mode=wandb_mode, resume=wandb_resume)
-    
+
     if args.wandb.resume_from != "None":
         wandb_init_config = {
             "mode": args.wandb.mode,
@@ -128,27 +97,17 @@ def main():
                 
         print(f"Resuming wandb run id: {wandb_logger.experiment.id}")   
     else:
-        wandb.init(project=wandb_project_name, id=wandb_id, mode=wandb_mode, resume=wandb_resume, config=args)
-        print(f"New wandb run id: {wandb_logger.experiment.id}")
-    
+        pass
+
     if args.wandb.save_code:
         wandb_logger.experiment.save("main_default.py")
-        # Get the directory of the current file
         current_dir = os.path.dirname(__file__)
-
-        # Construct the relative path
         relative_path = os.path.join(current_dir, "exp_main.py")
-
-        # Save the file using the relative path
         wandb_logger.experiment.save(relative_path)
-    
-        # Get the current Git commit ID
         commit_id = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode('utf-8')
-
-        # Log the current Git commit ID
         wandb_logger.experiment.config.update({"git_commit_id": commit_id})
         print(f"Git commit ID: {commit_id}")
-        
+
     if args.wandb.resume != "None" or args.wandb.resume_from != "None":
         def log_config_diffs(old_config, new_config, step):
             diffs = {}
@@ -166,36 +125,49 @@ def main():
         wandb_logger.experiment.config.update(args, allow_val_change=True)
         new_config = dict(wandb_logger.experiment.config)
         log_config_diffs(old_config, new_config, step="update_args")
-        
-        
+
+    return wandb_logger
+                                
+                                
+def main():
+    args = parse_args(CONFIG_FILENAME)
+    pprint(vars(args))
+    config_dict = args.configs.to_dict()
+    random_seed = config_dict['general']['random_seed']
     pl.seed_everything(random_seed)
-    
+
+    wandb_logger = init_wandb_logger(args)
+
+    model = config_dict['training']['model_info']['model']
+    dataset = config_dict['general']['dataset']
+    features = config_dict['general']['features']
+    context_len = config_dict['training']['sequence']['label_len']
+    label_len = config_dict['training']['sequence']['pred_len']
+    pred_len = config_dict['training']['sequence']['pred_len']
+    learning_rate = config_dict['optimization']['learning_rate']
+    batch_size = config_dict['optimization']['batch_size']
+    inverse = config_dict['data']['inverse']
+    iterations = config_dict['training']['iterations']['itr']
+    tag = config_dict['general']['tag']
+
     for iteration in range(iterations):
-        # setting record of experiments
-
-        # random seed
-        # fix_seed = iteration if iterations > 1 else random_seed
-
         setting = f"{model}_{dataset}_ft{features}_sl{context_len}_ll{label_len}_pl{pred_len}_lr{learning_rate}_bs{batch_size}_inv{inverse}_itr{iteration}"
-        
         if tag is not None:
             setting += f"_{tag}"
 
         time_now = time.time()
-            
         str_time_now = time.strftime("%d_%m_%Y_%H%M", time.localtime(time_now))
         model_start_training_time = str_time_now
         args.paths.checkpoints = os.path.join(args.paths.checkpoints, setting, model_start_training_time)
         exp = ExpMainLightning(args)
-        
+
         print(f'>>>>>>>start training : {setting}>>>>>>>>>>>>>>>>>>>>>>>>>')
         with Debbuger(debug=args.debug):
             trainer = Trainer(
                 devices=2,
                 accelerator="cuda",
                 max_epochs=args.training.iterations.train_epochs,
-                num_sanity_val_steps=0,  # Disable sanity check
-                # use_distributed_sampler = True,
+                num_sanity_val_steps=0,
                 logger=wandb_logger,
                 callbacks=[
                     CustomModelCheckpoint(
@@ -215,18 +187,14 @@ def main():
                     ModelSummary(max_depth=3)
                 ],
                 strategy=FSDPStrategy(
-                                        sharding_strategy="FULL_SHARD",
-                                        # cpu_offload=True,
-                                      ), #DDPStrategy(find_unused_parameters=True)
+                    sharding_strategy="FULL_SHARD",
+                ),
             )
             trainer.fit(exp)
 
         print(f'>>>>>>>testing : {setting}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
         trainer.test(exp)
-        
         torch.cuda.empty_cache()
-        
-    
 
 if __name__ == "__main__":
     main()
